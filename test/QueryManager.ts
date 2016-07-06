32e0c81122082412cb85d88eb1b0eae2383028a6
amandajliu <ajliu72@gmail.com>
2016-07-05 17:23:02 -0700

    Moved methods, still debugging failed tests

--- test/QueryManager.ts
+++ test/QueryManager.ts
@@ -1,7 +1,12 @@
 import {
   QueryManager,
   QuerySubscription,
+  ObservableQuery,
 } from '../src/QueryManager';
+
+import {
+  Observer
+} from '../src/util/Observable';
 
 import {
   createApolloStore,
@@ -557,13 +562,13 @@ describe('QueryManager', () => {
       variables,
     });
 
-    const subscription = handle.subscribe({
+    handle.subscribe({
       next(result) {
         handleCount++;
 
         if (handleCount === 1) {
           assert.deepEqual(result.data, data1);
-          subscription.refetch();
+          handle.refetch();
         } else if (handleCount === 2) {
           assert.deepEqual(result.data, data2);
           done();
@@ -614,9 +619,9 @@ describe('QueryManager', () => {
       query,
     });
 
-    const subscription = handle.subscribe({});
+    handle.subscribe({});
 
-    subscription.refetch().then((result) => {
+    handle.refetch().then((result) => {
       assert.deepEqual(result.data, data2);
       done();
     });
@@ -680,16 +685,16 @@ describe('QueryManager', () => {
       query: query,
     });
 
-    const subscription = handle.subscribe({
+    handle.subscribe({
       next(result) {
         handleCount++;
 
         if (handleCount === 1) {
           assert.deepEqual(result.data, data1);
-          subscription.refetch();
+          handle.refetch();
         } else if (handleCount === 2) {
           assert.deepEqual(result.data, data2);
-          subscription.refetch(variables);
+          handle.refetch(variables);
         } else if (handleCount === 3) {
           assert.deepEqual(result.data, data3);
           done();
@@ -753,18 +758,18 @@ describe('QueryManager', () => {
 
     let resultCount = 0;
 
-    const sub = handle.subscribe({
+    handle.subscribe({
       next(result) {
         resultCount++;
         // Perform refetch on first result from watchQuery
         if (resultCount === 1) {
-          sub.refetch();
+          handle.refetch();
         };
 
         // Wait for a result count of 3
         if (resultCount === 3) {
           // Stop polling
-          sub.stopPolling();
+          handle.stopPolling();
           assert(result);
           done();
         }
@@ -871,7 +876,7 @@ describe('QueryManager', () => {
       });
 
       // Refetch before we get any data - maybe the network is slow, and the user clicked refresh?
-      subscription.refetch();
+      handle.refetch();
     });
   });
 
@@ -1301,12 +1306,12 @@ describe('QueryManager', () => {
     });
 
     let handleCount = 0;
-    const subscription = handle.subscribe({
+    handle.subscribe({
       next(result) {
         handleCount++;
         if (handleCount === 1) {
           assert.deepEqual(result.data, data1);
-          return subscription.refetch();
+          return handle.refetch();
         } else if (handleCount === 2) {
           assert.deepEqual(result.data, data2);
           store.dispatch({
@@ -1875,7 +1880,7 @@ describe('QueryManager', () => {
       },
     });
 
-    subscription.startPolling(50);
+    handle.startPolling(50);
   });
   it('exposes a way to stop a polling query', (done) => {
     const query = gql`
@@ -1927,12 +1932,12 @@ describe('QueryManager', () => {
       pollInterval: 50,
     });
 
-    const subscription = handle.subscribe({
+    handle.subscribe({
       next(result) {
         handleCount++;
 
         if (handleCount === 2) {
-          subscription.stopPolling();
+          handle.stopPolling();
         }
       },
     });
@@ -2271,33 +2276,20 @@ describe('QueryManager', () => {
       queryManager.resetStore();
     });
 
-    it('should call refetch on a mocked QuerySubscription if the store is reset', (done) => {
-      const mockQuerySubscription: QuerySubscription = {
-        unsubscribe() {
-          done(new Error('Unsubscribe was called on a subscription on store reset.'));
-        },
-
+    it('should call refetch on a mocked Observable if the store is reset', (done) => {
+      const mockObservableQuery: ObservableQuery = {
         refetch(variables: any): Promise<GraphQLResult> {
           done();
           return null;
         },
-
-        stopPolling(): void {
-          done(new Error('stopPolling was called on a subscription on store reset.'));
-        },
-
-        startPolling(pollInterval): void {
-          done(new Error('startPolling was called on a subscription on a store reset.'));
-        },
-      };
+      } as ObservableQuery;
       const queryManager = new QueryManager({
         networkInterface: mockNetworkInterface(),
         store: createApolloStore(),
         reduxRootKey: 'apollo',
       });
       const queryId = 'super-fake-id';
-      queryManager.addObservableQuery(queryId, null);
-      queryManager.addQuerySubscription(queryId, mockQuerySubscription);
+      queryManager.addObservableQuery(queryId, mockObservableQuery);
       queryManager.resetStore();
     });
 
